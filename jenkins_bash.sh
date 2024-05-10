850d9fca31599819401eea5d40d0651d7a069441
Josh Gray <joshgray91@gmail.com>
2024-05-10 15:32:32 +0100

    Update jenkins_bash.sh

--- jenkins_bash.sh
+++ jenkins_bash.sh
@@ -1,6 +1,8 @@
+EC2_IP=18.201.193.156
+
 # bypass key checking step/option
 # ssh into ec2
-ssh -o  "StrictHostKeyChecking=no" ubuntu@52.50.223.145 <<EOF
+ssh -o  "StrictHostKeyChecking=no" ubuntu@$EC2_IP <<EOF
 # run update and upgrade
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -8,40 +10,14 @@ sudo apt-get upgrade -y
 EOF
 
 # copy new code
-rsync -avz -e "ssh -o StrictHostKeyChecking=no" app ubuntu@52.50.223.145:/home/ubuntu
-rsync -avz -e "ssh -o StrictHostKeyChecking=no" environment ubuntu@52.50.223.145:/home/ubuntu
+rsync -avz -e "ssh -o StrictHostKeyChecking=no" app ubuntu@$EC2_IP:/home/ubuntu
+rsync -avz -e "ssh -o StrictHostKeyChecking=no" environment ubuntu@$EC2_IP:/home/ubuntu
 
-ssh -o  "StrictHostKeyChecking=no" ubuntu@52.50.223.145 <<EOF
-# Install node
-curl -fsSL https://deb.nodesource.com/setup_10.x | sudo DEBIAN_FRONTEND=noninteractive -E bash - && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
+ssh -o  "StrictHostKeyChecking=no" ubuntu@$EC2_IP <<EOF
+# cd into env app folder
+cd environment/app
 
-# install nginx
-sudo apt-get install nginx -y
-sudo systemctl enable nginx
-
-# install npm
-sudo apt install npm -y
-
-# cd into app folder
-cd app
-
-# Install node packages
-npm install
-
-# Install pm2
-sudo npm install pm2 -g
-
-# stop any previously running versions of the app
-pm2 stop app
-
-# launch app
-pm2 start app.js 
-
+chmod +x provision.sh
+./provision.sh
 
 EOF
-
-# navigate to the folder containing the script 
-# install the required dependencies using provision.sh
-# navigate to app folder
-# run node packages with npm
-# start app in the background
